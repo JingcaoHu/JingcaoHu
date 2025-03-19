69b82d03a0fd3ac0be887214bed4e36224cb5a96
JingcaoHu <kylelyk1234567890@gmail.com>
2025-03-19 11:36:12 -0400

    Personal Website

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const toggleButton = document.createElement('button');
+toggleButton.innerText = 'Toggle Dark Mode';
+document.body.appendChild(toggleButton);
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
